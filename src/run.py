a9a953ab22b3963f336e6eeb7ce5596077d35c05
Piyush Singh <piyush.singh.01123@gmail.com>
2022-06-29 10:12:36 +0000

    some progress

--- src/run.py
+++ src/run.py
@@ -6,16 +6,18 @@ from stitch import stitch_multiple_images_fast,stitch_multiple_images
 from postprocess import dilate_and_save
 from utils import plot_image
 
-# folder_path = "data/images/Image Mosaicing"
+folder_path = "data/images/Image Mosaicing"
 # image_paths = [os.path.join(folder_path,file_name) for file_name in ["1_2.jpg", "1_4.jpg", "1_3.jpg", "1_1.jpg"]]
 # img_mosaic = stitch_multiple_images_fast(image_paths, prefix="set1")
 # cv2.imwrite("results/mosaic_set1.jpg", img_mosaic)
 
-# fp = "results/mosaic_set1.jpg"
-# fp_dilated = dilate_and_save(fp)
-# plot_image(fp_dilated)
+# plot_image(dilate_and_save("results/mosaic_set1.jpg"))
 
 
-image_paths = glob.glob("data/images/Image Mosaicing/2_*.*")
-img_mosaic = stitch_multiple_images(image_paths, prefix="set2", max_iter=5000, threshold=2)
-cv2.imwrite("results/mosaic_set2.jpg", img_mosaic)
+# image_paths = glob.glob("data/images/Image Mosaicing/2_*.*")
+# img_mosaic = stitch_multiple_images(image_paths, prefix="set2", max_iter=5000, threshold=2)
+# cv2.imwrite("results/mosaic_set2.jpg", img_mosaic)
+
+image_paths = [os.path.join(folder_path,file_name) for file_name in ["3_1.png", "3_2.png"]]
+img_mosaic = stitch_multiple_images_fast(image_paths, prefix="set3", max_iter=5000, threshold=2)
+cv2.imwrite("results/mosaic_set3.jpg", img_mosaic)
