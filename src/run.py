7aa264324279f49a2254b89a2265225b98463937
Piyush Singh <piyush.singh.01123@gmail.com>
2022-06-29 10:18:02 +0000

    some progress

--- src/run.py
+++ src/run.py
@@ -21,4 +21,9 @@ folder_path = "data/images/Image Mosaicing"
 # image_paths = [os.path.join(folder_path,file_name) for file_name in ["3_1.png", "3_2.png"]]
 # img_mosaic = stitch_multiple_images_fast(image_paths, prefix="set3", max_iter=5000, threshold=2)
 # cv2.imwrite("results/mosaic_set3.jpg", img_mosaic)
-plot_image(dilate_and_save("results/mosaic_set3.jpg"))
+# plot_image(dilate_and_save("results/mosaic_set3.jpg"))
+
+
+image_paths = [os.path.join(folder_path,file_name) for file_name in ["4_2.jpg", "4_1.jpg"]]
+img_mosaic = stitch_multiple_images_fast(image_paths, prefix="set4")
+cv2.imwrite("results/mosaic_set4.jpg", img_mosaic)
